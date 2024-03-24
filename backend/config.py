d5e02b0fc72a7b9188adf39b2804a596df635d86
Randike <djay0011@student.monash.edu>
2024-03-24 15:49:09 +0530

    backend 80% complete now. video at https://youtu.be/PppslXOR7TA?t=2279. USE POSTMAN TO TEST API BEFORE CONTINUING

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask import Flask     # Importing our framework Flask
+from flask_sqlalchemy import SQLAlchemy     # ORM used to interface with our SQL db
+from flask_cors import CORS     # This module is used to allow Cross Origin Resource Shariing because we want our database and frontend to live on separate servers 
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(__name__) # initiating our flask app
+CORS(app) # enabling cross origin resource sharing in our app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # linking our app to the location that the database is stored in
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Not tracking modifications on our database
+db = SQLAlchemy(app) # initializing a database for our app
 
---
+++ backend/models.py
@@ -0,0 +1,20 @@
+from config import db # importing our database model from config
+class Contact(db.Model):
+# defining the structure of our database 
+    id = db.Column(db.Integer, primary_key=True)    # the databases primary key
+    first_name = db.Column(db.String(80),  unique=False, nullable=False) # first name non-unique and required
+    last_name = db.Column(db.String(80),  unique=False, nullable=False) # last name non-unique and required
+    email = db.Column(db.String(120),  unique=True, nullable=False) # email unique and required
+    phone =db.Column(db.String(11), unique =True, nullable=True)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email,
+            "phone": self.phone
+        }
+
+    # def getNumber(self):
+    #     return self.phone

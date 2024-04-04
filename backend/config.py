aba2685815049e94e29b561b19b8c6dc779aaadd
Randike <djay0011@student.monash.edu>
2024-04-04 19:16:23 +0530

    added some formatting changes

--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,6 @@ from flask import Flask     # Importing our framework Flask
 from models import db
 from flask_cors import CORS     # This module is used to allow Cross Origin Resource Shariing because we want our database and frontend to live on separate servers 
 
-# app = Flask(__name__) # initiating our flask app
-# CORS(app) # enabling cross origin resource sharing in our app
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # linking our app to the location that the database is stored in
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Not tracking modifications on our database
-# db = SQLAlchemy(app) # initializing a database for our app
-
 class ConfigApp:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'  # linking our app to the location that the database
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Not tracking modifications on our database
@@ -22,6 +15,5 @@ def createApp(app_config = ConfigApp):
     CORS(app) # enabling cross origin resource sharing in our app
     app.config.from_object(app_config)
     db.init_app(app)
-    
     
     return app, db
